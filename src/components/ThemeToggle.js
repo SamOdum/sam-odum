b4b3795505b47c5e628626a2dd661847fc816bcb
Sam Odum <sam.odum1@gmail.com>
2020-04-06 14:40:46 +0100

    Make ThemeToggle component follow Eslint rules
    Because:
    - The rules makes for bug-free code
    - Encourages uniformity of code style across projecg
    - And enforces tested and trusted standards
    
    I re-saved the ThemeToggle component and forced it to follow the rules
    established by Eslint.

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const ThemeToggle = () => {
-    return ( 
-        <div className="wrapper">
-            <input type="checkbox" id="hide-checkbox"/>
-            <label for="hide-checkbox" className="toggle">
-                <span className="toggle-button">
-                <span className="crater crater-1"></span>
-                <span className="crater crater-2"></span>
-                <span className="crater crater-3"></span>   
-                <span className="crater crater-4"></span>
-                <span className="crater crater-5"></span>
-                <span className="crater crater-6"></span>
-                <span className="crater crater-7"></span>
-                </span>
-                <span className="star star-1"></span>
-                <span className="star star-2"></span>
-                <span className="star star-3"></span>
-                <span className="star star-4"></span>
-                <span className="star star-5"></span>
-                <span className="star star-6"></span>
-                <span className="star star-7"></span>
-                <span className="star star-8"></span>
-            </label>
-        </div>
-     );
-}
- 
+  return (
+    <div className="wrapper">
+      <input type="checkbox" id="hide-checkbox" />
+      <label htmlFor="hide-checkbox" className="toggle">
+        <span className="toggle-button">
+          <span className="crater crater-1" />
+          <span className="crater crater-2" />
+          <span className="crater crater-3" />
+          <span className="crater crater-4" />
+          <span className="crater crater-5" />
+          <span className="crater crater-6" />
+          <span className="crater crater-7" />
+        </span>
+        <span className="star star-1" />
+        <span className="star star-2" />
+        <span className="star star-3" />
+        <span className="star star-4" />
+        <span className="star star-5" />
+        <span className="star star-6" />
+        <span className="star star-7" />
+        <span className="star star-8" />
+      </label>
+    </div>
+  );
+};
+
 export default ThemeToggle;
