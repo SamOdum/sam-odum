f090b110a0176e89b10270233d86f2a22f999c2c
Sam Odum <sam.odum1@gmail.com>
2020-04-15 03:08:53 +0100

    Flesh out contact page
    Because:
    - Former state was skeletal
    - I needed to flesh it out so it looks functional
    
    I built out the contact page with a form and a series of
    social media anchors that link to my social media.
    
    What I have is skeletal and needsmore work.
    
    I'll need to implement the form elements the react way.
    And I'll also need to refactor the hard style px values
    to use responsive and adaptive units, miuch like the rest
    of the project.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,87 +2,121 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="slide" id="4">
-      <div className="content fourth-content">
-        <div className="container-fluid">
-          <h1>Contact page</h1>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Contat = () => {
-  return (
     <div className="slide" id="5">
-      <div className="content fifth-content">
-        <div className="container-fluid">
-          <div id="map">
-            {/* -- How to change your own map point
-            1. Go to Google Maps
-            2. Click on your location point
-            3. Click "Share" and choose "Embed map" tab
-            4. Copy only URL and paste it within the src="" field below
-    --*/}
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3647.3030413476204!2d100.5641230193719!3d13.757206847615207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf51ce6427b7918fc!2sG+Tower!5e0!3m2!1sen!2sth!4v1510722015945"
-              width="100%"
-              height="500px"
-              frameBorder="0"
-              style="border:0"
-              allowFullScreen
+      <div className="content contact">
+        <div id="map" className="contact-image">
+          {/* How to change your own map point
+              1. Go to Google Maps
+              2. Click on your location point
+              3. Click "Share" and choose "Embed map" tab
+              4. Copy only URL and paste it within the src="" field below
+              */}
+          <iframe
+            src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3974.4420409999734!2d8.340729409255468!3d5.031833654537036!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x105d7d0efa569cb5%3A0xdc7d76ba7ce5ed9a!2sEmmanuel%20Edem%20Cres%2C%20Calabar!5e0!3m2!1sen!2sng!4v1586912424342!5m2!1sen!2sng" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"'
+            width="100%"
+            height="500px"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen
+            title="location"
+          />
+        </div>
+        <div className="contact-text">
+          <form id="contact" className="contact-text__form">
+            <input
+              name="name"
+              className="contact-text__form-field"
+              type="text"
+              id="name"
+              placeholder="Your name..."
+              required
             />
-          </div>
-          <div className="col-md-6">
-            <form id="contact" action="" method="post">
-              <div className="row">
-                <fieldset>
-                  <input
-                    name="name"
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Your name..."
-                    required=""
-                  />
-                </fieldset>
-                <fieldset>
-                  <input
-                    name="email"
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="Your email..."
-                    required=""
-                  />
-                </fieldset>
-                <fieldset>
-                  <input
-                    name="subject"
-                    type="text"
-                    className="form-control"
-                    id="subject"
-                    placeholder="Subject..."
-                    required=""
-                  />
-                </fieldset>
-                <fieldset>
-                  <textarea
-                    name="message"
-                    rows="6"
-                    className="form-control"
-                    id="message"
-                    placeholder="Your message..."
-                    required=""
-                  />
-                </fieldset>
-                <fieldset>
-                  <button type="submit" id="form-submit" className="btn">
-                    Send Now
-                  </button>
-                </fieldset>
-              </div>
-            </form>
+
+            <input
+              name="email"
+              className="contact-text__form-field"
+              type="email"
+              id="email"
+              placeholder="Your email..."
+              required
+            />
+
+            <input
+              name="subject"
+              className="contact-text__form-field"
+              type="text"
+              id="subject"
+              placeholder="Subject..."
+              required
+            />
+
+            <textarea
+              name="message"
+              className="contact-text__form-field"
+              rows="6"
+              id="message"
+              placeholder="Your message..."
+              required
+            />
+
+            <button
+              className="contact-text__form-btn"
+              id="form-submit"
+              Name="btn"
+              type="submit"
+            >
+              Send Now
+            </button>
+          </form>
+          <div className="contact__social">
+            <p>Connect with me on social media:</p>
+            <ul className="contact__social-icon-container">
+              <li className="contact__social-icon">
+                <a
+                  href="https://github.com/samodum"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="fab fa-github" />
+                </a>
+              </li>
+              <li className="contact__social-icon">
+                <a
+                  href="https://linkedin.com/in/samodum/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="fab fa-linkedin" />
+                </a>
+              </li>
+              <li className="contact__social-icon">
+                <a
+                  href="http://twitter.com/samodum1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="fab fa-twitter" />
+                </a>
+              </li>
+              <li className="contact__social-icon">
+                <a
+                  href="https://facebook.com/SamOdum2"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="fab fa-facebook" />
+                </a>
+              </li>
+              <li className="contact__social-icon">
+                <a
+                  href="https://dev.to/samodum"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="fab fa-dev" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
