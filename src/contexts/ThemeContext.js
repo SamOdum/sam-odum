deb54126df1b218c683db49dc8fd383c4732ed75
Sam Odum <sam.odum1@gmail.com>
2020-04-17 00:06:32 +0100

    Continuation of theme toggle
    Because:
    - It was a work in progress I left off
    
    I've now created its context and also a skeletal state.
    It's stil a WIP.

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,9 +1,15 @@
-import React, {createContext} from 'react'
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const ThemeContext = createContext();
 
-const ThemeProvider = () => {
-    return (  );
-}
- 
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState({});
+  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
 export default ThemeContext;
